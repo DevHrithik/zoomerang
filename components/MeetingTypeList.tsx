de82f1de67ada0deb765f010099e1c899cd69bae
Hrithik Roy <hrithikroy1854@gmail.com>
2024-07-24 23:28:30 +0530

    Added reuseable meeting modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import HomeCard from "./HomeCard";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
 
 export default function MettingTypeList() {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >(undefined);
+  const createMeeting = () => {};
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -38,6 +40,14 @@ export default function MettingTypeList() {
         description="Meeting Recordings"
         className="bg-yellow-1"
         handleClick={() => router.push("/recordings")}
+      />
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an instant meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
